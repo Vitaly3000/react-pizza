898d41afae946f86a66bd9829aef4d97cd70cd0b
Vitaly <65219683+Vitaly3000@users.noreply.github.com>
2021-01-27 04:48:31 +0300

    lesson 5

--- react-pizza/src/App.js
+++ react-pizza/src/App.js
@@ -1,15 +1,30 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { Route } from 'react-router-dom';
 import { Header } from './components';
 import { Home, Cart } from './pages';
 
 function App() {
+  const [pizzas, setPizzas] = useState([]);
+  useEffect(() => {
+    axios
+      .get('http://localhost:3000/db.json')
+      .then(({ data }) => setPizzas(data.pizzas));
+    // fetch('http://localhost:3000/db.json').then((response) => {
+    //   console.log(
+    //     response.json().then((pizzas) => {
+    //       setPizzas(pizzas.pizzas);
+    //     }),
+    //   );
+    // });
+  }, []);
   return (
     <div className="App">
       <div className="wrapper">
         <Header />
         <div className="content">
-          <Route path="/" component={Home} exact />
-          <Route path="/cart" component={Cart} exact/>
+          <Route path="/" render={() => <Home items={pizzas} />} exact />
+          <Route path="/cart" component={Cart} exact />
         </div>
       </div>
     </div>
