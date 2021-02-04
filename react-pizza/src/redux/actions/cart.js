3e15120cc73707751ee3d080b28f559253911c8d
Vitaly <65219683+Vitaly3000@users.noreply.github.com>
2021-02-04 20:17:39 +0300

    finish

--- react-pizza/src/redux/actions/cart.js
+++ react-pizza/src/redux/actions/cart.js
@@ -1,6 +1,25 @@
 const ADD_PIZZA_CART = 'ADD_PIZZA_CART';
-
+const CLEAR_CART = 'CLEAR_CART';
+const REMOVE_CART_ITEM = 'REMOVE_CART_ITEM';
+const PLUS_CART_ITEM = 'PLUS_CART_ITEM';
+const MINUS_CART_ITEM = 'MINUS_CART_ITEM';
 export const addPizzaToCart = (pizzaObj) => ({
   type: ADD_PIZZA_CART,
   payload: pizzaObj,
+});
+
+export const clearCart = () => ({
+  type: CLEAR_CART,
+});
+export const removeCartItem = (id) => ({
+  type: REMOVE_CART_ITEM,
+  payload: id,
+});
+export const minusCartItem = (id) => ({
+  type: MINUS_CART_ITEM,
+  payload: id,
+});
+export const plusCartItem = (id) => ({
+  type: PLUS_CART_ITEM,
+  payload: id,
 });
